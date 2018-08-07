6b35ca895806bd904f3092c9acb416d59cb5df3b
YossiKer <jozf120@gmail.com>
2018-08-07 19:05:45 +0300

    add anime form logic

--- Client/src/app/Components/content/add-anime-form/add-anime-form.component.ts
+++ Client/src/app/Components/content/add-anime-form/add-anime-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { Anime } from '../../../Objects/Anime';
+import { AnimesService } from '../../../Services/animes.service';
 
 @Component({
   selector: 'app-add-anime-form',
@@ -8,18 +10,48 @@ import { FormBuilder } from '@angular/forms';
 })
 export class AddAnimeFormComponent implements OnInit {
 
+  seenLatestEpisode: boolean;
+
   animeAdditionForm = this.fb.group({
     anime_name: ['Enter the anime name'],
-    current_episode: ['Enter the current episode']
+    current_episode: ['Enter the current episode'],
+    seen_latest_episode: false
   });
 
-  constructor(private fb: FormBuilder) { }
+  animes: Anime[];
+
+  constructor(private fb: FormBuilder, private animesService: AnimesService) { }
 
   ngOnInit() {
+    this.seenLatestEpisode = false;
+
+    this.animesService.getAnimes().subscribe(
+      (animes: Anime[]) => {
+        this.animes = animes;
+        console.log(this.animes);
+      }, 
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
   sendAddedAnime() {
     console.log(this.animeAdditionForm.value);
   }
 
+  checkboxChanged(animeName: string) {
+    this.seenLatestEpisode = !this.seenLatestEpisode;
+
+    let control: FormControl = (<FormControl>this.animeAdditionForm.get('current_episode'));
+    
+    if (this.seenLatestEpisode) {
+      if (animeName.length != 0) {
+        control.setValue(this.animes.find(anime => anime.anime_name === animeName).latest_episode);
+        control.disable();
+      }
+    } else {
+      control.enable();
+    }
+  }
 }
